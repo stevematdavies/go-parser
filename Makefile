c6a2d5ced7a3d3fb1556ab97d9c55a60bb7ed884
Stephen Davies <stephen.davies@ixonos.com>
2016-10-06 01:15:44 +0300

    working beta

--- Makefile
+++ Makefile
@@ -1,23 +1,8 @@
-BINARY=gomake
+build:
+		go build
 
-# These are the values you want to pass to the build
-VERSION=1.0.0
-BUILD=`git rev-parse HEAD`
-
-# Set up the -ldflags option for go, interpolate the values
-LDFLAGS=-ldflags "-X main.Version=${VERSION} -X main.Build=${BUILD}"
-
-.DEFAULT_GOAL:	${BINARY}
-
-${BINARY}:
-		go build ${LDFLAGS} -o ${BINARY} ./..
-
-# installs nescessary binaries
 install:
-	go install ${LDFLAGS} -o ${BINARY} ./..
-
-# cleans out projects cleans binaries
-.PHONY: clean install
+	go install
 
 clean:
-	if [ -f ${BINARY} ] ; then rm ${BINARY} ; fi
+	go clean
--- main.go
+++ main.go
@@ -1,13 +1,73 @@
 package main
 
-import "fmt"
-
-var (
-	Version string
-	Build   string
+import (
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
 )
 
+const path = "tmp/.pmc.out"
+
+const MockString = `key1: 490942    key2: 0:0:0
+    key3: 0:0:23    key4: 2:232:32`
+
 func main() {
-	fmt.Println("Version: ", Version)
-	fmt.Println("Git Commit Hash: ", Build)
+	CreateFile()
+	WriteFile()
+	ReadFile()
+}
+
+func getData() string {
+	out, err := exec.Command("syncappmmi.sh").Output()
+	check(err)
+	return string(out)
+}
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func CreateFile() {
+	_, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		file, err := os.Create(path)
+		check(err)
+		file.Close()
+	}
+}
+
+func WriteFile() {
+	var file, err = os.OpenFile(path, os.O_RDWR, 0644)
+	check(err)
+	_, err = file.WriteString(getData())
+	check(err)
+	err = file.Sync()
+	check(err)
+	file.Close()
+}
+
+func ReadFile() {
+	var file, err = os.OpenFile(path, os.O_RDWR, 0644)
+	check(err)
+	defer file.Close()
+	out := make([]byte, 1024)
+	for {
+		n, err := file.Read(out)
+		if err != io.EOF {
+			check(err)
+		}
+		if n == 0 {
+			break
+		}
+	}
+	file.Close()
+	Parse(string(out))
+	check(err)
+}
+
+func Parse(s string) {
+	fmt.Println(s)
 }
